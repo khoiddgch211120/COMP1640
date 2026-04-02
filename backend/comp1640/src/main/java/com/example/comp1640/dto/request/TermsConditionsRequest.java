709871be00d822d1f2fcc21a5ac978ec45c70a62
khoiddgch211120 <khoiddgch211120@fpt.edu.vn>
2026-04-03 01:58:42 +0700

    fix: terms condition conflict

--- backend/comp1640/src/main/java/com/example/comp1640/dto/request/TermsConditionsRequest.java
+++ backend/comp1640/src/main/java/com/example/comp1640/dto/request/TermsConditionsRequest.java
@@ -1,37 +1,19 @@
 package com.example.comp1640.dto.request;
 
-<<<<<<< HEAD
 import java.time.LocalDate;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-=======
-<<<<<<<< HEAD:backend/comp1640/src/main/java/com/example/comp1640/dto/request/TermsconditionsRequest.java
-import java.time.LocalDate;
-
-import lombok.Getter;
-========
 import lombok.AllArgsConstructor;
 import lombok.Data;
->>>>>>>> 43f3aebc0c95dc7738436e7e1bd8d1951d6d7cab:backend/comp1640/src/main/java/com/example/comp1640/dto/request/TermsConditionsRequest.java
-import lombok.NoArgsConstructor;
 
-<<<<<<<< HEAD:backend/comp1640/src/main/java/com/example/comp1640/dto/request/TermsconditionsRequest.java
 @Getter
 @Setter
-========
-import java.time.LocalDate;
-
 @Data
->>>>>>>> 43f3aebc0c95dc7738436e7e1bd8d1951d6d7cab:backend/comp1640/src/main/java/com/example/comp1640/dto/request/TermsConditionsRequest.java
-@NoArgsConstructor
 @AllArgsConstructor
->>>>>>> 43f3aebc0c95dc7738436e7e1bd8d1951d6d7cab
+
 public class TermsConditionsRequest {
 
     private String version;
